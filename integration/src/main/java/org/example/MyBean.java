a8981c12aec83ae48010ac4f7776d958ee355d16
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-02-15 09:34:27 +0100

    init 🌱

---
+++ integration/src/main/java/org/example/MyBean.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MyBean {
+
+    private String hi;
+    private String bye;
+
+    public MyBean(String hi, String bye) {
+        this.hi = hi;
+        this.bye = bye;
+    }
+
+    public String hello() {
+        return hi + " how are you?";
+    }
+
+    public String bye() {
+        return bye + " World";
+    }
+}
