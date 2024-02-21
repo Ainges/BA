addd76f82441509f28bc5bf9849173817c9367c0
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-02-21 20:24:08 +0100

    🚧 added first Camel Route

--- integration/src/main/java/com/hubertusseitz/MyApplication.java
+++ integration/src/main/java/com/hubertusseitz/MyApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.hubertusseitz;
 
 import org.apache.camel.main.Main;
 
--- integration/src/test/java/com/hubertusseitz/MyApplicationTest.java
+++ integration/src/test/java/com/hubertusseitz/MyApplicationTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.hubertusseitz;
 
 import java.util.concurrent.TimeUnit;
 
