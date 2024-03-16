997addc807dd4bff61f480177defc916ad1f102e
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-03-16 21:17:27 +0100

    ♻️ 💄 Refactor ExampleEmployeeList module CSS and add employee card selection functionality

--- processSuite/apps/processSuite/frontend/src/ExampleEmployeeList/ExampleEmployeeList.tsx
+++ processSuite/apps/processSuite/frontend/src/ExampleEmployeeList/ExampleEmployeeList.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { CustomFormProps } from "../DialogRenderer";
-import { Avatar, Card, Col, DatePicker, Layout, Row } from "antd";
+import {
+  Avatar,
+  Card,
+  Checkbox,
+  Col,
+  DatePicker,
+  Layout,
+  Row,
+  Typography,
+} from "antd";
 import axios from "axios";
 import styles from "./ExampleEmployeeList.module.css";
 import Meta from "antd/es/card/Meta";
+import { SettingOutlined, PlusOutlined } from "@ant-design/icons";
 
 const ExampleEmployeeList: React.FC<CustomFormProps> = () => {
   const { Header, Footer, Sider, Content } = Layout;
@@ -19,7 +29,14 @@ const ExampleEmployeeList: React.FC<CustomFormProps> = () => {
 
   const employeeAPIgetAllurl = "http://localhost:8080/employee/all";
   const test = "https://reqres.in/api/users?page=2";
+
+  // ### React State ###
+
+  // Array of Employees
   const [employees, setEmployees] = useState<Employee[]>([]);
+
+  // State of the selected cards
+  const [cardselected, setcardSelected] = useState<String[]>([]);
 
   useEffect(() => {
     const fetchEmployees = async () => {
@@ -31,48 +48,56 @@ const ExampleEmployeeList: React.FC<CustomFormProps> = () => {
     fetchEmployees();
   }, []);
 
-  //   const [dimensions, setDimensions] = useState({
-  //     width: window.innerWidth,
-  //     height: window.innerHeight,
-  //   });
-  //   useEffect(() => {
-  //     const updateDimensions = () => {
-  //       setDimensions({
-  //         width: window.innerWidth,
-  //         height: window.innerHeight,
-  //       });
-  //     };
-
-  //     window.addEventListener("resize", updateDimensions);
-
-  //     return () => {
-  //       window.removeEventListener("resize", updateDimensions);
-  //     };
-  //   }, []);
+  const handleEmployeeCardClick = (employeeUsername: string) => {
+    if (cardselected.includes(employeeUsername)) {
+      // If the employee is already selected, remove them from the array
+      console.log("Unselecting Employee " + employeeUsername);
+      setcardSelected(
+        cardselected.filter((username) => username !== employeeUsername)
+      );
+    } else {
+      // If the employee is not selected, add them to the array
+      console.log("Selecting Employee " + employeeUsername);
+      setcardSelected([...cardselected, employeeUsername]);
+    }
+    console.log("Click ");
+  };
 
   return (
     <>
-      {/* <div style={{ width: dimensions.width, height: dimensions.height }}> */}
-      <Layout className={styles.layoutStyle}>
-        <Header className={styles.headerStyle}>Header</Header>
-        <Content className={styles.contentStyle}>
-          <div className={styles.cardContainerStlye}>
-            <Card title="Mitarbeiter"  className={styles.cardContainerStlye}>
-              {employees.map((employee) => (
-                <Card key={employee.username}>
-                  <Meta
-                    avatar={<Avatar size={100} src={employee.pictureURI} />}
-                    title={employee.fullName}
-                    description={employee.companyAndPosition}
-                  />
-                </Card>
-              ))}
+      {/* <Card className={styles.instructionCard}>
+        <Typography.Title level={4}>Mitarbeiter Auswahl</Typography.Title>
+        <Typography.Paragraph>
+          Wählen Sie die Mitarbeiter aus, mit denen der neue Miarbeiter ein Gesprächstermin ausmachen soll.
+        </Typography.Paragraph>
+      </Card> */}
+      <div className={styles.outerStyle}>
+        <Card className={styles.cardContainerStyle} title="Mitarbeiter">
+          {employees.map((employee) => (
+            // <Card className={styles.employeeOuterCard}>
+            //   <div onClick={() => alert("Hello from here")}>
+            <Card
+              className={
+                cardselected.includes(employee.username)
+                  ? styles.employeeCardSelected
+                  : styles.employeeCard
+              }
+              key={employee.username}
+              onClick={() => handleEmployeeCardClick(employee.username)}
+              actions={[<PlusOutlined selected key="add" />]}
+            >
+              <Meta
+                avatar={<Avatar size={100} src={employee.pictureURI} />}
+                title={employee.fullName}
+                description={employee.companyAndPosition}
+              />
             </Card>
-          </div>
-        </Content>
-        <Footer className={styles.footerStyle}>Footer</Footer>
-      </Layout>
-      {/* </div> */}
+            //   </div>
+            //   <Checkbox className={styles.checkboxStyle}></Checkbox>
+            // </Card>
+          ))}
+        </Card>
+      </div>
     </>
   );
 };
