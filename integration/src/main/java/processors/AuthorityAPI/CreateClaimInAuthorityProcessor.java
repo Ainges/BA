bfae3a54e4a231554824eeeacca95b1720787a68
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-05-11 16:59:30 +0200

    🌱 begun Create Claim Route

---
+++ integration/src/main/java/processors/AuthorityAPI/CreateClaimInAuthorityProcessor.java
@@ -0,0 +1,16 @@
+package processors.AuthorityAPI;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+@ApplicationScoped
+public class CreateClaimInAuthorityProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        
+
+
+    }
+}
