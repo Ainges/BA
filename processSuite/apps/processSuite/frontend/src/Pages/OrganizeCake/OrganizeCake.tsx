d342cd687d170148887281fa3ce1259ab276c9c2
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-05-13 23:46:37 +0200

    ✨💄 Implemented OrganizeSmallPresent

--- processSuite/apps/processSuite/frontend/src/Pages/OrganizeCake/OrganizeCake.tsx
+++ processSuite/apps/processSuite/frontend/src/Pages/OrganizeCake/OrganizeCake.tsx
@@ -1,83 +1,19 @@
-import { Row, Divider, Col, Card, Button, Space, Checkbox } from "antd";
+import { UserTaskResult } from "@5minds/processcube_engine_sdk";
+import { FormState } from "@atlas-engine-contrib/atlas-ui_sdk";
+import OrganizeTask from "../../Components/OrganizeTask/OrganizeTask";
 import { CustomFormProps } from "../../DialogRenderer";
-import styles from "./OrganizeCake.module.css";
-import { EmployeeData } from "../../Components/EmployeeData/EmployeeData";
-import { useEffect, useState } from "react";
-import { title } from "process";
-import { format } from "path";
-import { formatDate } from "../../functions/formatDate";
 import Paragraph from "antd/es/typography/Paragraph";
-
-interface InewEmployeeDataType {
-  OnboardingData: {
-    first_name: string;
-    last_name: string;
-    first_working_day: string;
-    birth_date: string;
-    position: string;
-  };
-}
-
-function isNewEmployeeData(obj: any): obj is InewEmployeeDataType {
-  return (
-    "OnboardingData" in obj &&
-    "first_name" in obj.OnboardingData &&
-    "last_name" in obj.OnboardingData &&
-    "first_working_day" in obj.OnboardingData &&
-    "birth_date" in obj.OnboardingData &&
-    "position" in obj.OnboardingData
-  );
-}
+import { formatDate } from "../../functions/formatDate";
 
 const OrganizeCake: React.FC<CustomFormProps> = (props) => {
-  const [newEmployeeData, setNewEmployeeData] = useState<EmployeeData[]>([]);
-  const [checkboxChecked, setCheckboxChecked] = useState<boolean>(false);
   const currentToken = props.userTask.startToken;
-
-  useEffect(() => {
-    const currentToken = props.userTask.startToken;
-
-    if (!isNewEmployeeData(currentToken)) {
-      console.error("Invalid ProcessData!");
-      return;
-    }
-
-    try {
-      const extractedData = [
-        {
-          title: "Name",
-          data:
-            currentToken.OnboardingData.first_name +
-            currentToken.OnboardingData.last_name,
-        },
-        {
-          title: "Geburtstag",
-          data: formatDate(currentToken.OnboardingData.birth_date),
-        },
-        {
-          title: "Erster Arbeitstag",
-          data: formatDate(currentToken.OnboardingData.first_working_day),
-        },
-        {
-          title: "Position",
-          data: currentToken.OnboardingData.position,
-        },
-      ];
-
-      setNewEmployeeData(extractedData);
-    } catch (error) {
-      console.error("Error while extracting data: " + error);
-    }
-  }, []);
   return (
     <>
-      <Row>
-        <Divider orientation="center">
-          Kuchen für Mitarbeiter organisieren
-        </Divider>
-
-        <Col span={18} className={styles.column}>
-          <Card title="Aufgabe">
+      <OrganizeTask
+        processData={props}
+        title={"Kuchen für Mitarbeiter organisieren"}
+        task={
+          <>
             <Paragraph>
               Der neue Mitarbeiter {currentToken.OnboardingData.first_name}{" "}
               {currentToken.OnboardingData.last_name} hat am{" "}
@@ -88,45 +24,13 @@ const OrganizeCake: React.FC<CustomFormProps> = (props) => {
               Gleichzeitig hat er am selben Tag Geburtstag. Bitte organisieren
               Sie einen Kuchen für den neuen Mitarbeiter!
             </Paragraph>
-            <Checkbox
-              checked={checkboxChecked}
-              onClick={() => {
-                setCheckboxChecked(!checkboxChecked);
-              }}
-            >
-              Kuchen ist auf den{" "}
-              {formatDate(currentToken.OnboardingData.first_working_day)}{" "}
-              bestellt!
-            </Checkbox>
-          </Card>
-        </Col>
-        <Col span={6} className={styles.column}>
-          <EmployeeData employeeData={newEmployeeData} />
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Space style={{ width: "100%", justifyContent: "center" }}>
-            <Button
-              type="primary"
-              disabled={!checkboxChecked}
-              onClick={() => {
-                props.finishUserTask({ result: "Cake organized" });
-              }}
-            >
-              Abschließen
-            </Button>
-            <Button
-              type="default"
-              onClick={() => {
-                props.suspendUserTask(props.suspendState);
-              }}
-            >
-              Zurückstellen
-            </Button>
-          </Space>
-        </Col>
-      </Row>
+          </>
+        }
+        checkboxText={`Kuchen ist auf den ${formatDate(
+          currentToken.OnboardingData.first_working_day
+        )} bestellt!`}
+        resultString={"Cake organized!"}
+      ></OrganizeTask>
     </>
   );
 };
--- processSuite/apps/processSuite/frontend/src/Pages/OrganizeSmallPresent/OrganizeSmallPresent.tsx
+++ processSuite/apps/processSuite/frontend/src/Pages/OrganizeSmallPresent/OrganizeSmallPresent.tsx
@@ -1,10 +1,48 @@
-import { Row } from "antd";
+import { Alert, Card, Col, Divider, Row } from "antd";
 import { CustomFormProps } from "../../DialogRenderer";
+import styles from "./OrganizeSmallPresent.module.css";
+import OrganizeTask from "../../Components/OrganizeTask/OrganizeTask";
+import { UserTaskResult } from "@5minds/processcube_engine_sdk";
+import { FormState } from "@atlas-engine-contrib/atlas-ui_sdk";
+import Paragraph from "antd/es/typography/Paragraph";
+import { formatDate } from "../../functions/formatDate";
 
 const OrganizeSmallPresent: React.FC<CustomFormProps> = (props) => {
+  const currentToken = props.userTask.startToken;
   return (
     <>
-      <Row></Row>
+      <OrganizeTask
+        processData={props}
+        title={`Eine kleine Aufmerksamkeit für ${currentToken.OnboardingData.first_name} ${currentToken.OnboardingData.last_name} organisieren`}
+        task={
+          <>
+            <Paragraph>
+              Der neue Mitarbeiter {currentToken.OnboardingData.first_name}{" "}
+              {currentToken.OnboardingData.last_name} hat am{" "}
+              {formatDate(currentToken.OnboardingData.first_working_day)} seinen
+              ersten Arbeitstag.
+            </Paragraph>
+            <Paragraph>
+              Er hat noch vor seinem ersten Arbeitstag Geburtstag. Bitte
+              organisieren Sie eine kleine Aufmerksamkeit für den neuen
+              Mitarbeiter!
+            </Paragraph>
+            <Alert
+              type="warning"
+              banner
+              message="Achten Sie bitte darauf, dass das Geschenk nicht zu früh zugestellt wird!"
+            ></Alert>
+            <Alert
+              type="warning"
+              banner
+              message="Achten Sie bitte außerdem darauf, dass der Wert den Betrag von 10€ nicht übersteigt!"
+            ></Alert>
+            <br></br>
+          </>
+        }
+        checkboxText={`Es wurde eine kleine Aufmerksamkeit and den neuen Mitarbeiter geschickt!`}
+        resultString={"Present send!"}
+      ></OrganizeTask>
     </>
   );
 };
