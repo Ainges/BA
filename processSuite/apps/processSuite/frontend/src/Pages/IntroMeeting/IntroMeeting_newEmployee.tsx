05364fb5fcb79b0cc7fac41c73719873533f64df
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-06-05 21:30:12 +0200

    💄 UI IntroMeeting_newEmployee done

--- processSuite/apps/processSuite/frontend/src/Pages/IntroMeeting/IntroMeeting_newEmployee.tsx
+++ processSuite/apps/processSuite/frontend/src/Pages/IntroMeeting/IntroMeeting_newEmployee.tsx
@@ -1,30 +1,77 @@
-import { Alert, Card, Col, Divider, Row } from "antd";
+import { Alert, Button, Card, Col, Divider, Row } from "antd";
 import { CustomFormProps } from "../../DialogRenderer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface IEmployee {
+  email: string;
+  first_name: string;
+  last_name: string;
+  position: string;
+  profile_picture_url: string;
+}
 
 const IntroMeeting_newEmployee: React.FC<CustomFormProps> = (props) => {
+  const [employee, setEmployee] = useState<IEmployee>();
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:8080/api/canonical/user/byMail/michael.scott@dundermifflin.com"
+      )
+      .then((response) => {
+        setEmployee(response.data);
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <>
       <Divider orientation="center">Kennenlerngespräch</Divider>
-      <Alert
-        type="info"
-        message={
-          <>
-            {" "}
-            <p>
-              Sie haben heute um{" "}
-              <strong> {props.userTask.startToken.time} Uhr </strong> ein
-              Kennenlerngespräch mit{" "}
-            </p>{" "}
-          </>
-        }
-      />
-      <br />
       <Row>
-        <Col span={1}></Col>
-        <Col span={22}>
-          <Card></Card>
+        {" "}
+        <Col span={6}></Col>
+        <Col span={12}>
+          <Alert
+            type="info"
+            showIcon
+            message="Info"
+            description={
+              <>
+                {" "}
+                <p>
+                  Sie haben heute um{" "}
+                  <strong> {props.userTask.startToken.time} Uhr </strong> ein
+                  Kennenlerngespräch mit{" "}
+                  <strong>
+                    {" "}
+                    {employee?.first_name} {employee?.last_name}
+                  </strong>
+                  .
+                </p>{" "}
+              </>
+            }
+          />
         </Col>
-        <Col span={1}></Col>
+        <Col span={6}></Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col span={6}></Col>
+        <Col span={12}>
+          <Button
+            type="primary"
+            onClick={() => {
+              props.finishUserTask({
+                done: true,
+              });
+            }}
+            block
+          >
+            Erledigt
+          </Button>
+        </Col>
+        <Col span={6}></Col>
       </Row>
     </>
   );
